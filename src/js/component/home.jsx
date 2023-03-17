2613cb256e20fc99e8056eb4b96841c958506304
BilelMatallah <bilelmatallah@hotmail.com>
2023-03-17 13:55:12 +0000

    feat: commit 3

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,9 +11,15 @@ const Home = () => {
 
 	return (
 		<div className="trafficLight container">
-			<div className="light red"></div>
-			<div className="light orange"></div>
-			<div className="light green"></div>			
+			<div onClick={() => setColor("red")} 
+			className= {"light red" + (color === "red" ? " activado" : "")}></div>
+			
+			<div onClick={() => setColor("orange")} 
+			className={"light orange" + (color === "orange" ? " activado" : "")}></div>
+			
+			
+			<div onClick={() => setColor ("green")} 
+			className={"light green" + (color === "green" ? " activado" : "")}></div>			
 		</div>
 	);
 };
